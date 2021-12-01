0e67f4094817e23a1806af87ecb0b5db0fde0601
Karlo Kotrba <karlo@constructor.io>
2021-12-01 18:00:48 +0100

    unhardcode version

--- build_cnstrc_manylinux_wheel.sh
+++ build_cnstrc_manylinux_wheel.sh
@@ -1,4 +1,6 @@
 ./build_cnstrc_wheel.sh
 
+version=`sed -n 's/^\s*version='\''\(.*\)'\'',$/\1/p' setup.py`
+
 # repair wheels to be manylinux
-auditwheel repair wheels/cnstrc_kenlm-0.0.2.dev1-cp38-cp38-linux_x86_64.whl -w wheels/manylinux
+auditwheel repair wheels/cnstrc_kenlm-$version-cp38-cp38-linux_x86_64.whl -w wheels/manylinux
