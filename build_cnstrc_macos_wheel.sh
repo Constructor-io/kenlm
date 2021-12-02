389d598d8d9bc4c92e2d8bf5b209523dc194cb6c
Karlo Kotrba <karlo@constructor.io>
2021-12-02 13:08:27 +0100

    added tmp folder

--- build_cnstrc_macos_wheel.sh
+++ build_cnstrc_macos_wheel.sh
@@ -1,4 +1,4 @@
 ./build_cnstrc_wheel.sh
 
 # repair wheels to have embedded libraries
-delocate-wheel -w wheels/macos wheels/cnstrc_kenlm*macosx*.whl
+delocate-wheel -w wheels/macos wheels/tmp/cnstrc_kenlm*macosx*.whl
--- build_cnstrc_manylinux_wheel.sh
+++ build_cnstrc_manylinux_wheel.sh
@@ -1,4 +1,4 @@
 ./build_cnstrc_wheel.sh
 
 # repair wheels to be manylinux
-auditwheel repair wheels/cnstrc_kenlm*linux_x86_64.whl -w wheels/manylinux
+auditwheel repair wheels/tmp/cnstrc_kenlm*linux_x86_64.whl -w wheels/manylinux
